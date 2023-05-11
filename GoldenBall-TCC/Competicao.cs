6e8692795ff875ba9d354a3df7cbfdc185b32b29
Daniel Maia <daniel_silva.maia@hotmail.com>
2023-05-10 21:08:07 -0300

    Add partida

--- GoldenBall-TCC/Competicao.cs
+++ GoldenBall-TCC/Competicao.cs
@@ -6,10 +6,36 @@
         {
             for (int i = 0; i < quantidadeTemporadas; i++)
             {
-                //foreach (var time in times)
-                //Time.TreinarJogador(time.Jogadores)
+                
             }
         }
 
+        public static void Partida(Time time1 , Time time2)
+        {
+            int golsTime1 = 0;
+            int golsTime2 = 0;
+
+            for (int i = 0; i < time1.Jogadores.Count; i++)
+            {
+                if (time1.Jogadores[i].Rota.Distancia < time2.Jogadores[i].Rota.Distancia)
+                    golsTime1++;
+                else if(time1.Jogadores[i].Rota.Distancia == time2.Jogadores[i].Rota.Distancia)
+                {
+                    golsTime1++;
+                    golsTime2++;
+                }
+                else
+                    golsTime2++;
+            }
+            if(golsTime1 > golsTime2)
+                time1.Pontuacao += 3;
+            else if(golsTime1 < golsTime2)
+                time2.Pontuacao += 3;
+            else
+            {
+                time1.Pontuacao += 1;
+                time2.Pontuacao += 1;
+            }
+        }
     }
 }
